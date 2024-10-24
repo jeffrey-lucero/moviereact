db34b25a707e5f80675dbed709cc0b8ee1c591e0
Jeffrey Lucero <Jeffrey.lucero@amaphil.com>
2024-10-24 18:00:05 +0800

    4th commit

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import GlobalApi from '../Services/GlobalApi';
 import MovieCard from './MovieCard';
 import { IoChevronBackOutline, IoChevronForward } from 'react-icons/io5';
 
 function MovieList({ genreId }) {
 	const [movieList, setMovieList] = useState([]);
+	const elementRef = useRef(null);
 	useEffect(() => {
 		getMovieByGenreId();
 	}, []);
@@ -14,13 +15,33 @@ function MovieList({ genreId }) {
 			setMovieList(resp.data.results);
 		});
 	};
+	const SliderRight = (element) => {
+		element.scrollLeft += 500;
+	};
+	const SliderLeft = (element) => {
+		element.scrollLeft -= 500;
+	};
 	return (
-		<div className="flex overflow-x-auto gap-8 pt-5 px-3 pb-5 scrollbar-hide scroll-smooth ">
-			{movieList.map((item, i) => (
-				<div className="flex-shrink-0 ">
-					<MovieCard movie={item} />
-				</div>
-			))}
+		<div className="relative">
+			<IoChevronBackOutline
+				className=" text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 top-0 absolute left-0 mt-[150px] "
+				onClick={() => SliderLeft(elementRef.current)}
+			/>
+
+			<div
+				ref={elementRef}
+				className="flex overflow-x-auto gap-8 pt-5 px-3 pb-5 scrollbar-hide scroll-smooth "
+			>
+				{movieList.map((item, i) => (
+					<div className="flex-shrink-0 ">
+						<MovieCard movie={item} />
+					</div>
+				))}
+			</div>
+			<IoChevronForward
+				className=" text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 top-0  absolute right-0 mt-[150px] "
+				onClick={() => SliderRight(elementRef.current)}
+			/>
 		</div>
 	);
 }
